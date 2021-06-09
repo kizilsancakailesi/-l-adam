db7086d69bbb8fcef8e425e7cee95a9557f9d7f3
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-09 15:01:50 +0300

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Merhaba 😎,, nasılsın şidmi şöyleki bu Userbir bot sohbeti.\n\n⚠️Rules:\n- Sohbet etmek yasak bilgileri Okuyunuz\n-Mesaj yazma spam sayılır \n\n 👉 **MÜZİK BOTUNU GRUPLARINIZA ALMAK İÇİN /KATİL KOMUTUNA BASINIZ İLK YAPMANIZ GEREKEN BOTU GRUBUNUZA EKLEMEK** @TurkishVoicebot**Denemeye değer 🤔**\n\n**Bilgileri okudunuz. KİŞİSEL BİLGİLERİ PAYLAŞMAYINIZ**\n\n**🤖 Developed by @EfsaneStar**")
+  await USER.send_message(message.chat.id,"Merhaba 😎,, nasılsın şidmi şöyleki bu Userbir bot sohbeti.\n\n⚠️Rules:\n- Sohbet etmek yasak bilgileri Okuyunuz\n-Mesaj yazma spam sayılır \n\n 👉 **MÜZİK BOTUNU GRUPLARINIZA ALMAK İÇİN /KATİL KOMUTUNA BASINIZ İLK YAPMANIZ GEREKEN BOTU GRUBUNUZA EKLEMEK** @TurkishVoicebot** Denemeye değer 🤔**\n\n**Bilgileri okudunuz. KİŞİSEL BİLGİLERİ PAYLAŞMAYINIZ**\n\n**🤖 Developed by @EfsaneStar**")
   return                        
