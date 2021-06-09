455fe8c55bd14b34b22ae145258a9207bcf00d75
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-09 14:47:39 +0300

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Halo,, ini adalah assistent service dari @zeldamusicbot .\n\n⚠️Rules:\n- Dilarang Chat Disini\n-Dilarang Spam Disini \n\n 👉 **KIRIM TAUTAN GROUP ANDA KESINI ATAU UNDANG** @zeldamusicbot **KE GROUP ANDA**\n\n**JANGAN LUPA JOIN** @Friendlycircle\n\n**🤖 Developed by @oppaidasukii**")
+  await USER.send_message(message.chat.id,"Merhaba 😎,, nasılsın şidmi şöyleki bu Userbir bot sohbeti.\n\n⚠️Rules:\n- Sohbet etmek yasak bilgileri Okuyunuz\n-Mesaj yazma spam sayılır \n\n 👉 **MÜZİK BOTUNU GRUPLARINIZA ALMAK İÇİN /KATİL KOMUTUNA BASINIZ İLK YAPMANIZ GEREKEN BOTU GRUBUNUZA EKLEMEK** @TurkishVoicebot**Denemeye değer 🤔**\n\n**Bilgileri okudunuz. KİŞİSEL BİLGİLERİ PAYLAŞMAYINIZ**\n\n**🤖 Developed by @EfsaneStar**")
   return                        
