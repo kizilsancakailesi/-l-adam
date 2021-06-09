e8302f6d916987c0e5d6680d3c85a51326c35ce2
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-09 09:38:13 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -80,24 +80,24 @@ async def start(client: Client, message: Message):
 async def help(client: Client, message: Message):
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name}!
-\n/play <song name> - play song you requested
+\n/oynat <song name> - play song you requested
 /dplay <song name> - play song you requested via deezer
 /splay <song name> - play song you requested via jio saavn
-/playlist - Show now playing list
+/oynatlist - Show now playing list
 /current - Show now playing
-/song <song name> - download songs you want quickly
-/search <query> - search videos on youtube with details
+/indir <song name> - download songs you want quickly
+/arama <query> - search videos on youtube with details
 /deezer <song name> - download songs you want quickly via deezer
 /saavn <song name> - download songs you want quickly via saavn
 /video <song name> - download videos you want quickly
 \n*Admins only*
 /player - open music player settings panel
-/pause - pause song play
-/resume - resume song play
-/skip - play next song
-/end - stop music play
-/userbotjoin - invite assistant to your chat
-/admincache - Refresh admin list
+/durdur - pause song play
+/devam - resume song play
+/atla - play next song
+/bitir- stop music play
+/katil - invite assistant to your chat
+/ayril - Refresh admin list
  </b>""",
         reply_markup=InlineKeyboardMarkup(
             [
