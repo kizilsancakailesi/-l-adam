f4af4bbc2109e04a02349d3e05095abb12246763
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-07-13 19:42:01 +0300

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -32,7 +32,7 @@ def bul(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Şarkı yükleniyooo...')
+    m = message.reply('🔎 istek parçası aranıyor...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -61,7 +61,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Yükleme başarılı Efendim**'
+        rep = '**🎵 Yükleme başarılı**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
